681ffbb5a62caf1bf33ce8252efac777611b7bb5
Orazbay <you@biy.kz>
2024-03-17 15:35:10 +0500

    Добавил в Models класс User

---
+++ Smash Cats/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Smash_Cats.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
