0ed312dd4742bfb8de95efd826c97fb6cafe8f28
Werterwy <orazbaijoldasbaevich@gmail.com>
2024-05-16 16:19:34 +0500

    а также обновил Person контоллер

--- Smash Cats/Controllers/PersonalController.cs
+++ Smash Cats/Controllers/PersonalController.cs
@@ -21,23 +21,6 @@ namespace Smash_Cats.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
-        /*   // [HttpGet]
-           public IActionResult Index()
-           {
-               return View();
-           }
-
-           // [HttpPost]
-           public async Task<IActionResult> Index(User user)
-           {
-               if (user == null)
-               {
-                   // Обработка случая, когда пользовательские данные не переданы
-                   user = new User(); // или перенаправьте на другую страницу, если необходимо
-               }
-               return View(user);
-           }*/
-
         public async Task<IActionResult> Index()
         {
             var userJson = HttpContext.Session.GetString("User");
@@ -47,15 +30,6 @@ namespace Smash_Cats.Controllers
                 return View(user);
             }
             return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> SetUser()
-        {
-            User user = null;
-            var userJson = JsonConvert.SerializeObject(user);
-            TempData["User"] = userJson;
-            return RedirectToAction("Index");
         }
 
         /*public async Task<IActionResult> Index(*//*int id*//*  User user)
@@ -78,32 +52,6 @@ namespace Smash_Cats.Controllers
             return View(user);
         }*/
 
-        /*
-                [HttpGet]
-                public async Task<IActionResult> Index()
-                {
-                    var httpClient = _httpClientFactory.CreateClient();
-
-                    // запрос к API для получения данных пользователя
-                    var response = await httpClient.GetAsync("http://localhost:5235/api/Login");
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var content = await response.Content.ReadAsStringAsync();
-                        var users = JsonConvert.DeserializeObject<IEnumerable<User>>(content);
-
-                        // В этом примере я верну первого пользователя из коллекции
-                        var currentUser = users.FirstOrDefault();
-                        return View(currentUser);
-                    }
-                    else
-                    {
-                        _logger.LogError("Ошибка при получении данных пользователя с API. Код ошибки: {StatusCode}", response.StatusCode);
-
-                        // Пользователь не аутентифицирован, перенаправляем на страницу входа
-                        return RedirectToAction("Index", "Login");
-                    }
-                }*/
 
 
     }
